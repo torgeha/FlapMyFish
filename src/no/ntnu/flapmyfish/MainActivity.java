25a4320d31baacb86e929e306799e983558a39ce
gvikas <gvikas91@gmail.com>
2014-03-13 14:25:57 +0100

    Comment on a class MainActivity

--- src/no/ntnu/flapmyfish/MainActivity.java
+++ src/no/ntnu/flapmyfish/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		//This is a new Instance
 		game = new Game(this, null);
 		
 //		game.pushState(new MainMenuScreen());
