f5fd8ca548cc2c68a5193109902b90c879666e23
Audun Brandt <audundb@gmail.com>
2014-03-13 13:39:59 +0100

    a

---
+++ src/no/ntnu/flapmyfish/BackgroundLayer.java
@@ -0,0 +1,51 @@
+package no.ntnu.flapmyfish;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import sheep.game.Layer;
+import sheep.game.Sprite;
+import sheep.game.SpriteContainer;
+import sheep.math.BoundingBox;
+import android.graphics.Canvas;
+
+public class BackgroundLayer extends Layer implements SpriteContainer{
+
+	private ArrayList<Sprite> sprites;
+	private LinkedList<Sprite> kill;
+	
+	public BackgroundLayer() {
+		sprites = new ArrayList<Sprite>();
+		kill = new LinkedList<Sprite>();
+	}
+	
+	@Override
+	public void update(float dt) {
+		for (Sprite s : sprites) {
+			s.update(dt);
+		}
+		
+		for (Sprite s : kill) {
+			sprites.remove(s);
+		}
+		kill.clear();
+	}
+
+	@Override
+	public void draw(Canvas canvas, BoundingBox box) {
+		for (Sprite s : sprites) {
+			s.draw(canvas);
+		}
+	}
+	
+	public void addSprite(Sprite sprite) {
+		sprites.add(sprite);
+		sprite.setParent(this);
+	}
+
+	public void removeSprite(Sprite sprite) {
+		kill.add(sprite);
+		sprite.setParent(null);
+	}
+	
+}
