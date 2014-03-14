bede14ef79c7578b5906c19445378f7b2cb5be59
thorjs <thoski91@hotmail.com>
2014-03-14 10:44:53 +0100

    Added Sprite2 with methods to set relative size

---
+++ src/no/ntnu/flapmyfish/tokens/Sprite2.java
@@ -0,0 +1,55 @@
+package no.ntnu.flapmyfish.tokens;
+
+import no.ntnu.flapmyfish.Constants;
+import sheep.game.Sprite;
+import sheep.graphics.Image;
+
+public class Sprite2 extends Sprite
+{
+//	private float imageWidth;
+//	private float imageHeight;
+	float xRatio;
+	float yRatio;
+	private float imageRatio;
+	
+	public Sprite2(Image image)
+	{
+		super(image);
+		float imageWidth = image.getWidth();
+		float imageHeight = image.getHeight();
+		xRatio = imageWidth/Constants.WINDOW_WIDTH;
+		yRatio = imageHeight/Constants.WINDOW_HEIGHT;
+		imageRatio = imageWidth/imageHeight;
+	}
+	
+	/**
+	 * Sets the size of the sprite relative to the screen size.
+	 * 
+	 * @param width
+	 * @param height
+	 */
+	public void setSize(float width, float height)
+	{
+		setScale(width*xRatio, height*yRatio);
+	}
+	
+	/**
+	 * Sets the height of the sprite relative to the height of the screen.
+	 * The width is set to preserve the original aspect ratio.
+	 * @param height
+	 */
+	public void setSizeByHeight(float height)
+	{
+		float width = height*imageRatio*yRatio;
+		setScale(width, height*yRatio);
+	}
+	
+	/**
+	 * Returns the height of the sprite relative to the screen height.
+	 * @return
+	 */
+	public float getHeight()
+	{
+		return getScale().getY()/yRatio;
+	}
+}
